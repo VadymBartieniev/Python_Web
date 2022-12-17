13360062d05fd738e9dfee56709cb42f74bce2aa
VadymBartieniev <vadimbarka14@gmail.com>
2022-12-17 18:42:24 +0200

    lab_9
    lab_9 python web

---
+++ lab9/app/form/controller.py
@@ -0,0 +1,12 @@
+from flask import render_template, flash, session
+from .file_writer import writeToFile
+from .. import App
+
+
+from . import form_blueprint
+<<<<<<< HEAD
+=======
+
+
+
+>>>>>>> 42ba28b980b68c9f693730290171c0cccf0f1c47
