42ba28b980b68c9f693730290171c0cccf0f1c47
VadymBartieniev <115421785+VadymBartieniev@users.noreply.github.com>
2022-12-17 18:34:31 +0200

    Update controller.py

--- lab9/app/form/controller.py
+++ lab9/app/form/controller.py
@@ -1,5 +1,4 @@
 from flask import render_template, flash, session
-from .forms import LoginFormSecond
 from .file_writer import writeToFile
 from .. import App
 from .custom_validator import validate
