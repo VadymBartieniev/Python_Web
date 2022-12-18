d399decf2eea1cb889254d22f937966dd1bdf061
VadymBartieniev <vadimbarka14@gmail.com>
2022-12-18 23:28:18 +0200

    0

--- lab12/app/form/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-form_blueprint = Blueprint('form', __name__, template_folder="templates")
-
-from . import controller
--- lab12/app/form/controller.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, flash, session
-from .. import App
-
-
-from . import form_blueprint
-
-
-
-        
